a57f2dd53f967f7a541d7d010ef775a941b2f600
ingermarie04 <ingermarie04@gmail.com>
2025-02-17 14:01:00 +0100

    recognizing lights in scene

--- Assets/Scripts/LightInScene.cs
+++ Assets/Scripts/LightInScene.cs
@@ -1,4 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class LightInScene : MonoBehaviour
 {
@@ -6,22 +9,62 @@ public class LightInScene : MonoBehaviour
 
     private Texture2D tex;
     private Rect rectReadPicture;
+    bool scanActive = true;
+    Color pixel;
+    public Color whitestPixel;
+
+    List<Color> pixels = new List<Color>();
 
     private void Awake()
     {
+        //Makes a 2D texture
         tex = new Texture2D(rt.width, rt.height);
+
+        //stores texture size
         rectReadPicture = new Rect(0, 0, rt.width, rt.height);
     }
-    private void Update()
+    private void Start()
     {
-        RenderTexture.active = rt;
+        if (scanActive)
+        {
+            RenderTexture.active = rt;
 
-        tex.ReadPixels(rectReadPicture, 0, 0);
-        tex.Apply();
+            //reads rt
+            tex.ReadPixels(rectReadPicture, 0, 0);
+            tex.Apply();
 
-        Debug.Log(tex.GetPixel(0, 0));
+            Debug.Log(tex.GetPixel(0, 0));
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < 256; y++)
+                {
+                    pixels.Add(tex.GetPixel(x, y));
+                }
+            }
+            for (int i = 0; i < pixels.Count; i++)
+            {
+                if (whitestPixel == null)
+                {
+                    whitestPixel = pixels[i];
+                    break;
+                }
 
-        RenderTexture.active = null;
-        rt.Release();
+                if (IsBrighter(pixels[i], whitestPixel))
+                {
+                    whitestPixel = pixels[i];
+                }
+            }
+
+            RenderTexture.active = null;
+            rt.Release();
+        }
+    }
+
+    bool IsBrighter(Color toCheck, Color toCompare)
+    {
+        Color.RGBToHSV(toCheck, out float toCheckH, out float toCheckS, out float toCheckV);
+        Color.RGBToHSV(toCompare, out float toCompareH, out float toCompareS, out float toCompareV);
+
+        return toCheckV >= toCompareV;
     }
 }
