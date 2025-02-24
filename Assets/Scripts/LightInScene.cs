43c0d1d03f2865abbcd7fe94e5b0c7e05b17d0b6
ingermarie04 <ingermarie04@gmail.com>
2025-02-24 13:40:48 +0100

    lights but like the way varjo wants it

--- Assets/Scripts/LightInScene.cs
+++ Assets/Scripts/LightInScene.cs
@@ -1,73 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class LightInScene : MonoBehaviour
 {
-    public RenderTexture rt;
+    [SerializeField] bool environmentReflections;
+    [SerializeField] float reflectionRefreshRate;
+    [SerializeField] VolumeProfile skyboxProfile;
+    [SerializeField] Cubemap defaultSky;
+    [SerializeField] List<PlanarReflectionProbe> OnCubemapUpdate = new List<PlanarReflectionProbe>();
 
-    private Texture2D tex;
-    private Rect rectReadPicture;
-    bool scanActive = true;
-    Color pixel;
-    public Color lightSource;
+    private bool isUpdating = false;
 
-    List<Color> pixels = new List<Color>();
-
-    private void Awake()
+    void Start()
     {
-        //Makes a 2D texture
-        tex = new Texture2D(rt.width, rt.height);
-
-        //stores texture size
-        rectReadPicture = new Rect(0, 0, rt.width, rt.height);
-    }
-    private IEnumerator Start()
-    {
-        if (scanActive)
+        if (environmentReflections)
         {
-            yield return new WaitForSeconds(5);
-
-            RenderTexture.active = rt;
-
-            //reads rt
-            tex.ReadPixels(rectReadPicture, 0, 0);
-            tex.Apply();
-
-            Debug.Log(tex.GetPixel(0, 0));
-            for (int x = 0; x < 256; x++)
-            {
-                for (int y = 0; y < 256; y++)
-                {
-                    pixels.Add(tex.GetPixel(x, y));
-                }
-            }
-            for (int i = 0; i < pixels.Count; i++)
-            {
-                if (lightSource == null)
-                {
-                    lightSource = pixels[i];
-                    break;
-                }
-
-                if (IsBrighter(pixels[i], lightSource))
-                {
-                    lightSource = pixels[i];
-                    Debug.Log(lightSource);
-                }
-            }
-
-            RenderTexture.active = null;
-            rt.Release();
+            StartReflectionUpdates();
         }
     }
 
-    bool IsBrighter(Color toCheck, Color toCompare)
+    void OnEnable()
     {
-        Color.RGBToHSV(toCheck, out float toCheckH, out float toCheckS, out float toCheckV);
-        Color.RGBToHSV(toCompare, out float toCompareH, out float toCompareS, out float toCompareV);
+        if (environmentReflections)
+        {
+            StartReflectionUpdates();
+        }
+    }
 
-        return toCheckV >= toCompareV;
+    void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    public void StartReflectionUpdates()
+    {
+        if (!isUpdating)
+        {
+            StartCoroutine(UpdateReflections());
+        }
+    }
+
+    private IEnumerator UpdateReflections()
+    {
+        isUpdating = true;
+
+        while (environmentReflections)
+        {
+            // Update Skybox reflections
+            UpdateSkyboxReflection();
+
+            // Update Planar Reflection Probes
+            foreach (var probe in OnCubemapUpdate)
+            {
+                if (probe != null)
+                    probe.RequestRenderNextUpdate();
+            }
+
+            yield return new WaitForSeconds(reflectionRefreshRate);
+        }
+
+        isUpdating = false;
+    }
+
+    private void UpdateSkyboxReflection()
+    {
+        if (skyboxProfile != null)
+        {
+            Cubemap newSkybox = GetRealWorldSkybox();
+            if (newSkybox != null)
+            {
+                skyboxProfile.TryGet(out UnityEngine.Rendering.HighDefinition.HDRISky hdriSky);
+                if (hdriSky != null)
+                {
+                    hdriSky.hdriSky.overrideState = true;
+                    hdriSky.hdriSky.value = newSkybox;
+                }
+            }
+        }
+    }
+
+    private Cubemap GetRealWorldSkybox()
+    {
+        // Placeholder for grabbing Varjo XR-3 pass-through skybox data
+        // Varjo SDK might have a function to generate a cubemap from real-world light
+        
+        return defaultSky;
+    }
+
+    public void SetEnvironmentReflections(bool enable)
+    {
+        environmentReflections = enable;
+
+        if (enable)
+            StartReflectionUpdates();
+        else
+            StopAllCoroutines();
     }
 }
